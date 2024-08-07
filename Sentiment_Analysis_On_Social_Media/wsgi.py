a186785deff626c37bafb36bcfee86b409d95a79
Rachit More <rachitmore3@gmail.com>
2024-08-07 16:06:30 +0530

    File has been added
    Django project has been created
    django home app has been created
    machine learning training and prediction pipeline have been created

---
+++ Sentiment_Analysis_On_Social_Media/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for Sentiment_Analysis_On_Social_Media project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "Sentiment_Analysis_On_Social_Media.settings"
+)
+
+application = get_wsgi_application()
