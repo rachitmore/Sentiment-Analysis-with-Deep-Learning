89b9c6cb8e89e155b317794cc1e4a5288eeae71d
Rachit More <rachitmore3@gmail.com>
2024-08-09 20:47:02 +0530

    Security Configuration updated

--- Sentiment_Analysis_On_Social_Media/settings.py
+++ Sentiment_Analysis_On_Social_Media/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=e(kx854(o%#%i-y_4*jpo*#q^)xwlrnqk*c3xj7cd=xc_)8q$"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
